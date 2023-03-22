73acb27e2ca9b7516bdc85c4387e62440f807540
timpel301 <tim.pel301@gmail.com>
2023-03-22 21:46:40 +0100

    animated physics game

---
+++ README.md
@@ -0,0 +1,4 @@
+# ABOUT
+
+This is a web browser based game with physics using java script.
+Following FreeCodeCamp tutorial.
---
+++ script.js
@@ -0,0 +1,154 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
+    class Player {
+        constructor(game){
+            this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 50;
+            this.speedX = 0;
+            this.speedY = 0;
+            this.dx = 0;
+            this.dy = 0;
+            this.speedModyfier = 5;
+        }
+        draw(context){
+            context.beginPath();
+            context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
+            context.beginPath();
+            context.moveTo(this.collisionX, this.collisionY);
+            context.lineTo(this.game.mouse.x, this.game.mouse.y);
+            context.stroke();
+        }
+        update(){
+            this.dx = this.game.mouse.x - this.collisionX;
+            this.dy = this.game.mouse.y - this.collisionY;
+            const distance = Math.hypot(this.dy, this.dx);
+            if (distance > this.speedModyfier){
+            this.speedX = this.dx/distance || 0;
+            this.speedY = this.dy/distance || 0;
+            }
+            else {
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            this.collisionX += this.speedX * this.speedModyfier;
+            this.collisionY += this.speedY * this.speedModyfier;
+        }
+    }
+
+    class Obstacle {
+        constructor(game){
+            this.game = game;
+            this.collisionX = Math.random() * this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.collisionRadius = 60;
+            this.image = document.getElementById('obstacles');
+            this.spriteWidth = 250;
+            this.spriteHeight = 250;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5 - 70;
+            this.frameX = Math.floor(Math.random() * 4) * this.spriteWidth;
+            this.frameY = Math.floor(Math.random() * 3) * this.spriteHeight;
+        }
+        draw(context){
+            context.drawImage(this.image, this.frameX, this.frameY, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
+            context.beginPath();
+            context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
+        }
+    }
+    
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.topMargin = 260;
+            this.player = new Player(this);
+            this.numberOfObstacles = 10;
+            this.obstacles = [];
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+            //event listeners with ES6 arrow function to inherit from class
+            window.addEventListener('mousedown', (e) => {
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+            });
+            window.addEventListener('mouseup', (e) => {
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+            window.addEventListener('mousemove', (e) => {
+                if (this.mouse.pressed){
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                }
+            });
+        }
+        render(context){
+            this.player.draw(context);
+            this.player.update();
+            this.obstacles.forEach(obstacle => obstacle.draw(context));
+        }
+        init(){
+            let attempts = 0;
+            while (this.obstacles.length < this.numberOfObstacles && attempts < 500){
+                let testObstacle = new Obstacle(this);
+                let overlap = false;
+                this.obstacles.forEach(obstacle => {
+                    const dx = testObstacle.collisionX - obstacle.collisionX;
+                    const dy = testObstacle.collisionY - obstacle.collisionY;
+                    const distance = Math.hypot(dy, dx);
+                    const distanceBuffer = 150;
+                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius + distanceBuffer;
+                    if (distance < sumOfRadii){
+                        overlap = true;
+                    }
+                });
+                const margin = testObstacle.collisionRadius * 2;
+                if (!overlap && testObstacle.spriteX > 0 && testObstacle.spriteX < this.width - testObstacle.width && testObstacle.collisionY > this.topMargin + margin && testObstacle.collisionY < this.height) {
+                    this.obstacles.push(testObstacle);
+                }
+                attempts++;
+            }    
+        }
+    }
+
+    const game = new Game(canvas);
+    game.init();
+    console.log(game);
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        window.requestAnimationFrame(animate); //endless animation loop
+    }
+    animate();
+
+});
